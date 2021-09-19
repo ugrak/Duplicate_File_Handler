8a41c7dc9328b4e9f4d03c0532cda5d1cf3f14a1
ugrak <augrak@yandex.ru>
2021-09-19 04:53:50 +0300

    First stage

--- handler.py
+++ handler.py
@@ -1,1 +1,10 @@
-# write your code here
+import os
+import sys
+
+try:
+    my_path = sys.argv[1]
+    for root, dirs, files in os.walk(my_path, topdown=False):
+        for name in files:
+            print(os.path.join(my_path, root, name))
+except IndexError:
+    print('Directory is not specified')
