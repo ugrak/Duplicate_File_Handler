589e3988419dae7f755b2009a53e5cb983e84c99
ugrak <augrak@yandex.ru>
2021-09-19 05:22:18 +0300

    Some little fixes

--- handler.py
+++ handler.py
@@ -1,10 +1,11 @@
 import os
 import sys
 
-try:
+if len(sys.argv) < 2:
+    print('Directory is not specified')
+    sys.exit(1)
+else:
     my_path = sys.argv[1]
-    for root, dirs, files in os.walk(my_path, topdown=False):
+    for root, dirs, files, in os.walk(my_path, topdown=False):
         for name in files:
             print(os.path.join(my_path, root, name))
-except IndexError:
-    print('Directory is not specified')
