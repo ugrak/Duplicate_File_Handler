83c7ed79b16bb6afc799bf860f913eb68c82040d
ugrak <augrak@yandex.ru>
2021-09-25 21:44:38 +0300

    Added file comparison by hash, fixed errors in the previous version.

--- handler.py
+++ handler.py
@@ -1,11 +1,13 @@
 import os
 import sys
+import hashlib
 
 if len(sys.argv) < 2:
     print('Directory is not specified')
     sys.exit(1)
 else:
     my_dict = {}
+    replicas_list = []
     my_path = sys.argv[1]
     format = input('Enter file format:\n')
     print('Size sorting options:\n1. Descending\n2. Ascending')
@@ -25,13 +27,35 @@ else:
             if name.endswith(format):
                 file_size = os.path.getsize(os.path.join(root, name))
                 file_path = os.path.join(root, name)
-                try:
-                    my_dict[file_size].append(file_path)
-                except KeyError:
-                    my_dict[file_size] = [file_path]
+                with open(file_path, 'rb') as f:
+                    md5 = hashlib.md5(f.read()).hexdigest()
+                my_dict.setdefault(file_size, {})
+                my_dict[file_size].setdefault(md5, {})
+                if my_dict[file_size][md5] == {}:
+                    my_dict[file_size][md5] = []
+                my_dict[file_size][md5].append(file_path)
 
-for key in sorted(my_dict.keys(), reverse=sorting):
-    if len(my_dict[key]) > 1:
-        print('\n' + str(key) + ' bytes')
-        for item in my_dict[key]:
-            print(item)
+    for i in sorted(my_dict.keys(), reverse=sorting):
+        print('\n' + str(i) + ' bytes')
+        for key, value in my_dict[i].items():
+            for element in value:
+                print(element)
+
+    while True:
+        replica_check = input('\nCheck for duplicates?\n')
+        if replica_check.lower() == 'yes' or replica_check.lower() == 'no':
+            break
+        else:
+            print('Wrong option')
+    for key, value in sorted(my_dict.items(), reverse=sorting):
+        flag = True
+        for hash, hash_list in value.items():
+            if len(hash_list) > 1:
+                if flag:
+                    print('\n' + str(key) + ' bytes\nHash: ' + hash)
+                    flag = False
+                else:
+                    print('Hash: ' + hash)
+                for i in sorted(hash_list):
+                    replicas_list.append(i)
+                    print(f'{len(replicas_list)}. {i}')
